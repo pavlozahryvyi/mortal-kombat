c30a2d08d4e4a7959d5c350d48c2b3fc650ec5b8
pavlozahryvyi <zagryviy@gmail.com>
2023-06-12 12:39:09 +0300

    Continue developing first screen

--- src/MortalKombat.js
+++ src/MortalKombat.js
@@ -1,16 +1,31 @@
 import { FirstScreen } from "./components/FirtstScreen";
+import { SecondScreen } from "./components/SeconsScreen";
 import "./styles.css";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const dataContext = createContext({});
+export const GameContext = createContext({});
 
 export function MortalKombat() {
+  const [playerOneId, setPlayerOneId] = useState(null);
+  const [playerTwoId, setPlayerTwoId] = useState(null);
+  const [isShowSecondScreen, setIsShowSecondScreen] = useState(false);
+
   return (
-    <div className="app">
-      MORTAL COMBAT
-      <div className="main-block">
-        <FirstScreen />
+    <GameContext.Provider
+      value={{
+        playerOneId,
+        setPlayerOneId,
+        playerTwoId,
+        setPlayerTwoId,
+        setIsShowSecondScreen,
+      }}
+    >
+      <div className="app">
+        MORTAL COMBAT
+        <div className="main-block">
+          {isShowSecondScreen ? <SecondScreen /> : <FirstScreen />}
+        </div>
       </div>
-    </div>
+    </GameContext.Provider>
   );
 }
--- src/components/FirtstScreen/index.js
+++ src/components/FirtstScreen/index.js
@@ -1,35 +1,103 @@
 import "./styles.css";
 import { makeNumberArray } from "./utils";
 import { skins } from "./skins";
+import { useContext, useEffect, useState } from "react";
+import { GameContext } from "../../MortalKombat";
 
 export function FirstScreen() {
-  const handleClick = (id) => {
-    // console.log(e.target);
-    // console.log(e.currentTarget);
-    // e.stopPropagation();
-    console.log("---handle click", id);
+  const {
+    playerOneId,
+    setPlayerOneId,
+    playerTwoId,
+    setPlayerTwoId,
+    setIsShowSecondScreen,
+  } = useContext(GameContext);
+
+  const [selectedPlayerId, setSelectedPlayerId] = useState(0);
+
+  const handleKeyDown = (event) => {
+    const { key } = event;
+
+    let diff = selectedPlayerId;
+
+    console.log("---selectedPlayerId", selectedPlayerId);
+    console.log("diff before", diff);
+
+    if (key === "ArrowUp") {
+      diff = diff - 5;
+      if (selectedPlayerId === 0) diff = 10;
+      if (selectedPlayerId === 1) diff = 11;
+      if (selectedPlayerId === 2) diff = 12;
+      if (selectedPlayerId === 3) diff = 13;
+      if (selectedPlayerId === 3) diff = 14;
+    } else if (key === "ArrowLeft") {
+      diff = diff - 1;
+      if (selectedPlayerId === 0) diff = 4;
+      if (selectedPlayerId === 5) diff = 9;
+      if (selectedPlayerId === 10) diff = 14;
+    } else if (key === "ArrowDown") {
+      diff = diff + 5;
+      if (selectedPlayerId === 10) diff = 0;
+      if (selectedPlayerId === 11) diff = 1;
+      if (selectedPlayerId === 12) diff = 2;
+      if (selectedPlayerId === 13) diff = 3;
+      if (selectedPlayerId === 14) diff = 4;
+    } else if (key === "ArrowRight") {
+      diff = diff + 1;
+      if (selectedPlayerId === 4) diff = 0;
+      if (selectedPlayerId === 9) diff = 5;
+      if (selectedPlayerId === 14) diff = 10;
+    }
+
+    console.log("diff after", diff);
+
+    setSelectedPlayerId(diff);
   };
+
+  console.log("---selectedPlayerId", selectedPlayerId);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    let timerId = null;
+    if (playerTwoId !== null) {
+      timerId = setTimeout(() => setIsShowSecondScreen(true), 2000);
+    }
+    return () => {
+      if (timerId) clearTimeout(timerId);
+    };
+  });
 
   return (
     <>
-      <div className="first-character" />
+      <div className="selected-character">
+        {playerOneId !== null && (
+          <img src={skins[playerOneId]} className="selected-character-img" />
+        )}
+      </div>
       <div className="first-wrap">
         {makeNumberArray(15).map((el, idx) => (
-          <button
+          <div
             key={el}
             value={el}
-            onClick={() => handleClick(el)}
             className="character-btn"
+            disabled={playerTwoId !== null}
           >
-            <img
-              src={skins[idx]}
-              className="character-img"
-              onClick={(e) => handleClick(e)}
-            />
-          </button>
+            <img src={skins[idx]} className="character-img" />
+          </div>
         ))}
       </div>
-      <div className="second-character" />
+      <div className="selected-character">
+        {playerTwoId !== null && (
+          <img src={skins[playerTwoId]} className="selected-character-img" />
+        )}
+      </div>
     </>
   );
 }
